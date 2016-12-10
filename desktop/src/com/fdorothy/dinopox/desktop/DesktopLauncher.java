89a9fbb6d3022dac7854081b3b9e8929bbb2f5ab
Fredric Dorothy <fredric.dorothy@vipaar.com>
2016-12-10 11:24:13 -0600

    made gdx project

---
+++ desktop/src/com/fdorothy/dinopox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fdorothy.dinopox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fdorothy.dinopox.DinoPox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DinoPox(), config);
+	}
+}
