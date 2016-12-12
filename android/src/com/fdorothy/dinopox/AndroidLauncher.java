e0d312f700a084b6db6f6e283f9651cf3aa41104
Fredric Dorothy <fredric.dorothy@vipaar.com>
2016-12-11 19:00:47 -0600

    Fixed android launcher

--- android/src/com/fdorothy/dinopox/AndroidLauncher.java
+++ android/src/com/fdorothy/dinopox/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.fdorothy.dinopox.DinoPox;
+import com.fdorothy.dinopox.DinoGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new DinoPox(), config);
+		initialize(new DinoGame(), config);
 	}
 }
