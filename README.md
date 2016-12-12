ae50ba5352b0e80749226bd7456b75f726f12aa1
Fredric Dorothy <fredric.dorothy@vipaar.com>
2016-12-11 18:29:31 -0600

    Added splash screen

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# dinopocalypse
-ludumdare 37 game
+This games was for ludum dare 37 https://ldjam.com/ and made in about 48 hours
+
+2017, the dinopocalypse. Thunderlizards emerge from their subterranean slumber
+with their eyes on world domination. One man holds the key to cast them back
+to the center of the earth, but he is stranded in a single-room research lab
+in rural Alabama.
+
+Do you have what it takes to defeat General Ankyleesaurus's horde?
+
+Rules
+-----
+
+You must survive 9 waves of attacks from the thunderlizard horde. Each wave
+is separated by a 1 minute wait period for you to build up the defenses in
+your single-room research lab.
+
+Touch the screen to move the research around.
+
+Touch a thunderlizard to shoot at it. But be warned, you cannot shoot through blocks.
+
+Some thunderlizards take a few shots to kill.
+
+If you press the 'pickup' button in the lower-left color then you can pickup
+items that are laying around the area. You can only pickup one item at a time.
+After pushing the button click the item and you will move there and pick it up.
+
+You can press the 'drop' button the lower-left color if you have an item and
+want to drop it off somewhere. Click again on the screen to select where you
+want to drop it.
--- core/src/com/fdorothy/dinopox/MainMenuScreen.java
+++ core/src/com/fdorothy/dinopox/MainMenuScreen.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MainMenuScreen implements Screen {
 
@@ -18,6 +20,11 @@ public class MainMenuScreen implements Screen {
   }
 
 
+  public int print(SpriteBatch batch, String text, int line) {
+    game.res.font.draw(batch, text, 0.0f, game.res.height-18*(line+1));
+    return line + 1;
+  }
+
   @Override
   public void render(float delta) {
     Gdx.gl.glClearColor(0, 0.15f, 0, 1);
@@ -27,9 +34,29 @@ public class MainMenuScreen implements Screen {
     game.res.batch.setProjectionMatrix(camera.combined);
 
     game.res.batch.begin();
-    game.res.font.draw(game.res.batch, "Welcome to DinoPox", 100, 150);
-    game.res.font.draw(game.res.batch, "Tap anywhere to begin!", 100, 100);
-    game.res.batch.end();
+    SpriteBatch b = game.res.batch;
+    int offset = 0;
+    offset = print(b, "2017, the dinopocalypse. Thunderlizards emerge from their subterranean slumber", offset);
+    offset = print(b, "with their eyes on world domination. One man holds the key to cast them back", offset);
+    offset = print(b, "to the center of the earth, but he is stranded in a single-room research lab", offset);
+    offset = print(b, "in rural Alabama.", offset);
+    offset = print(b, "", offset);
+    offset = print(b, "Do you have what it takes to defeat General Ankyleesaurus's horde?", offset);
+    offset = print(b, "", offset);
+    offset = print(b, "Rules", offset);
+    offset = print(b, "", offset);
+    offset = print(b, " - You must survive 9 waves of attacks from the thunderlizard horde.", offset);
+    offset = print(b, " - Touch the screen to move the researcher around.", offset);
+    offset = print(b, " - Touch a thunderlizard to shoot at it, some are harder than others to kill", offset);
+    
+    b.draw(game.res.dinos[Resources.LEE], 0, 0);
+    TextureRegion t = game.res.dinos[Resources.PARA];
+    float w = t.getRegionWidth();
+    float h = t.getRegionHeight();
+    float x = game.res.width - t.getRegionWidth() / 2.0f;
+    b.draw(t, (x+w) - w / 2.0f, 0.0f, -w, h);
+
+    b.end();
 
     if (Gdx.input.isTouched()) {
       game.setScreen(new GameScreen(game));
